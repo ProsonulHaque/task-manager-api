6f86a8c98fe9f45969f9e70590f988e1709cbaeb
Prosonul Haque <proshonulhaq@gmail.com>
2025-09-10 06:59:52 +0600

    Added migrations

---
+++ Migrations/20250910005654_UpdatedColumnName.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace task_manager_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "create_date",
+                schema: "public",
+                table: "user_tasks",
+                newName: "create_date_utc");
+
+            migrationBuilder.RenameColumn(
+                name: "create_date",
+                schema: "public",
+                table: "statuses",
+                newName: "create_date_utc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "create_date_utc",
+                schema: "public",
+                table: "user_tasks",
+                newName: "create_date");
+
+            migrationBuilder.RenameColumn(
+                name: "create_date_utc",
+                schema: "public",
+                table: "statuses",
+                newName: "create_date");
+        }
+    }
+}
